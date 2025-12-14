c00bbfc374d2620a95bc4af3d3a79a6c55bc225f
GUYwithPTSD <ivandarma3000@gmail.com>
2025-12-14 16:22:27 +0700

    setting flutter ke supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_mobile1/page/homepage.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+const supabaseUrl = 'https://liyrkvktshqrctjkautu.supabase.co';
+const supabasekey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpeXJrdmt0c2hxcmN0amthdXR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2OTYwNjIsImV4cCI6MjA4MTI3MjA2Mn0.74Y7EZPXIwwwYFsqrJLAwLUtyQ4WGnIbjmq8WFmQ8yg';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabasekey);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
